5b3ac26e3217041d9d0ddc0275c5373a69fa67ef
Grant Schroeder <gschroeder@pznma.com>
2019-06-12 21:35:13 +0000

    input save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,16 @@ class ApplicationController < ActionController::Base
   def new
     @place = Place.new
   end
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end 
+
+private
+
+def place_params
+  params.require(:place).permit(:name, :description, :address)
+end
 
 
 end
