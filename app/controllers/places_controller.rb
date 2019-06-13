9dbfe5eee7ff070303e265d31689e7804c2dbcc6
Grant Schroeder <gschroeder@pznma.com>
2019-06-13 03:12:29 +0000

    edit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,6 +13,14 @@ before_action :authenticate_user!, only: [:new, :create]
   def show
     @place = Place.find(params[:id])
   end
+  def edit
+    @place = Place.find(params[:id])
+  end
+  def update
+    @place = Place.find(params[:id])
+    @place = update_attributes(place_params)
+    redirect_to root_path
+  end 
 
 private
 
