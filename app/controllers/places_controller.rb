fb2b0a2f9603d31b2aeebab0ca706dd4b80403ca
Grant Schroeder <gschroeder@pznma.com>
2019-06-13 01:28:34 +0000

    Place detail page update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,6 +10,9 @@ before_action :authenticate_user!, only: [:new, :create]
      current_user.places.create(place_params)
     redirect_to root_path
   end 
+  def show
+    @place = Place.find(params[:id])
+  end
 
 private
 
