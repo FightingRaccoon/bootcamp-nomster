c0c75a9fc9077976ace8d1e824f530c18229297f
Grant Schroeder <gschroeder@pznma.com>
2019-06-26 03:21:53 +0000

    Photo update

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,13 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photos
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: true
   validates :address, presence: true  
   validates :description, presence: true
+  
+  mount_uploader :picture, PictureUploader
   
 end
